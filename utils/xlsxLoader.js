4f3ca8d183fd26c1a8ede47163cf0375e13d1500
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-12-21 19:21:07 -0300

    Updated xlsxLoader

--- utils/xlsxLoader.js
+++ utils/xlsxLoader.js
@@ -7,13 +7,12 @@ const xlsxLoader = async (filePath) => {
 
 		const wb = read(ab);
 
-		const ws = wb.Sheets[wb.SheetNames[0]]; // get the first worksheet
-		const data = utils.sheet_to_json(ws); // generate objects
-
+		const ws = wb.Sheets[wb.SheetNames[0]];
+		const data = utils.sheet_to_json(ws);
 		return data;
 	} catch (error) {
 		console.error('Error fetching or processing data:', error);
-		throw error; // Re-throw the error to be caught by the calling code if needed
+		throw error;
 	}
 };
 
