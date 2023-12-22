702a920a82bb5d6d4b496c60fb873c7908ae593c
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-12-22 09:53:41 -0300

    Added smooth opening

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -3,40 +3,38 @@ import React, { useState } from 'react';
 import Chevron from './icons/chevron';
 
 const Dropdown = ({ title, children }) => {
+  const [active, setActive] = useState(false);
+  const hasChildren = children;
 
-	const [ active, setActive ] = useState(false);
-    const hasChildren = children;
-
-    const handleClick = () => {
-        if (hasChildren) {
-            setActive(!active);
-        }
+  const handleClick = () => {
+    if (hasChildren) {
+      setActive(!active);
     }
+  };
 
-    return (
-        <div className='w-100 ps-4 pb-1'>
-            <div
-                type={`${hasChildren ? 'button' : ''}`}
-                onClick={handleClick}
-                className={`d-flex justify-content-between align-items-center pt-2 border-bottom ${hasChildren ? 'dropdown__hover' : ''}`}
-                >
-                <span>{title}</span>
-                { hasChildren &&
-		        <div > 
-                    <Chevron size='16px' orientation={active ? 'down' : 'up'} />
-                </div>
-                }
-
-            </div>
-			{ active &&
-				(
-                    <div className="mb-4">
-            	    { children }
-                    </div>
-				)
-			}
-        </div>
-    );
-}
+  return (
+    <div className='w-100 ps-4 pb-1'>
+      <div
+        role={`${hasChildren ? 'button' : ''}`}
+        onClick={handleClick}
+        className={`d-flex justify-content-between align-items-center pt-2 border-bottom ${hasChildren ? 'dropdown__hover' : ''}`}
+      >
+        <span>{title}</span>
+        {hasChildren && (
+          <div>
+            <Chevron size='16px' orientation={active ? 'down' : 'up'} />
+          </div>
+        )}
+      </div>
+      <div
+        className={`overflow-hidden transition-max-height ${active ? 'max-height-open' : 'max-height-closed'}`}
+        style={{ maxHeight: active ? '500px' : '0px' }} // Adjust the max height as needed
+      >
+        {active && <div>{children}</div>}
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
+
